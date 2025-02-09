d4f6aa8d5af8a1a8c4809a70b2888cfd62287ec6
Aditya Maisuriya <adiiimaisuriya94@gmail.com>
2025-02-09 20:28:59 +0530

    JwtService.cs

---
+++ Services/JwtService.cs
@@ -0,0 +1,32 @@
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace EcommerceAPI.Services
+{
+    public class JwtService
+    {
+        private readonly string _secretKey;
+
+        public JwtService(string secretKey)
+        {
+            _secretKey = secretKey;
+        }
+
+        public string GenerateToken(string username)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_secretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, username) }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
